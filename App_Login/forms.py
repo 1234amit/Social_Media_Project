fe936586ed091f40fbf29d1f3be3e2b8360cb277
Amit Goswami <cseiubatgoswami@gmail.com>
2021-02-04 23:23:52 +0600

    first commit

---
+++ App_Login/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from App_Login.models import UserProfile
+
+class CreateNewUser(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Enter Username'}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Enter your password'}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Confirm your password'}))
+    class Meta:
+        model = User
+        fields = ('email','username','password1','password2')
+
+class EditProfile(forms.ModelForm):
+    dob = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
